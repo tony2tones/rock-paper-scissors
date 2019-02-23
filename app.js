b60365b0e705f6436ee798c16f48d1d0901eed48
Anthony <anthonymarques@outlook.com>
2019-02-23 12:03:30 +0200

    added computerchoice function

--- app.js
+++ app.js
@@ -3,7 +3,34 @@ const compScore = 0;
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector('.result');
-const rock_div = document.getElementById('r');
-const paper_div = document.getElementById('p');
-const scissors_div = document.getElementById('s');
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random()* 3);
+    return choices[randomNumber];
+}
+console.log(getComputerChoice());
+
+function game(userChoice){
+    console.log("poop "+ userChoice);
+}
+
+function main() {
+  rock_div.addEventListener("click", function() {
+    game("r");
+  });
+
+  paper_div.addEventListener("click", function() {
+    game("p");
+  });
+
+  scissors_div.addEventListener("click", function() {
+    game("s");
+  });
+}
+
+main();
